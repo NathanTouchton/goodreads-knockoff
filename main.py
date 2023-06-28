74827432a5014abb596d319abe5636e99e14742b
Nathan Touchton <ntouchton@protonmail.com>
2023-06-27 19:54:30 -0500

    new file:   instance/books.db 	modified:   main.py 	deleted:    poetry.lock 	deleted:    pyproject.toml 	modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
 
 all_books = []
+db = SQLAlchemy(app)
 
+
+class books(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(250), nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.FLOAT(), nullable=False)
+
+    def __init__(self, title, author, rating):
+        self.title = title
+        self.author = author
+        self.rating = rating
+
+# with app.app_context():
+    # db.create_all()
 
 @app.route('/')
 def home():
     print(all_books)
-    # TODO: Check why it's not showing the list on the homepage. Probably a Jinja syntax issue.
     return render_template("index.html", all_books=all_books)
 
 
